0dd916ddfa7de840d1439b45409f2a3a36e25bdf
ColtMcG1 <coltmcg0@gmail.com>
2022-09-22 19:00:24 -0600

    Add spdlog

---
+++ src/Catalyst/src/core/logging.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "external/spdlog/include/spdlog/common.h"
+
+namespace Catalyst
+{
+    namespace internal
+    {
+
+        class InternalSink : public spdlog::sinks::sink
+        {
+
+        };
+
+    }
+
+    class Sink
+    {
+
+    };
+
+}
